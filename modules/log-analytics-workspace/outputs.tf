dea8ced05e387879c00f7372897f7c80213cd56a
Luke Fritz <lukefritz@gmail.com>
2020-06-15 16:58:11 -0500

    Add name output; add output descriptions (patch)

--- modules/log-analytics-workspace/outputs.tf
+++ modules/log-analytics-workspace/outputs.tf
@@ -1,19 +1,26 @@
-output "id" {
-  value = azurerm_log_analytics_workspace.workspace.id
+output "name" {
+  description = "The name of the Log Analytics workspace."
+  value       = azurerm_log_analytics_workspace.workspace.name
 }
 
-output "workspace_id" {
-  value = azurerm_log_analytics_workspace.workspace.workspace_id
+output "id" {
+  description = "The ID of the Log Analytics workspace."
+  value       = azurerm_log_analytics_workspace.workspace.id
 }
 
 output "portal_url" {
-  value = azurerm_log_analytics_workspace.workspace.portal_url
+  description = "The Portal URL for the Log Analytics workspace."
+  value       = azurerm_log_analytics_workspace.workspace.portal_url
 }
 
 output "primary_shared_key" {
-  value = azurerm_log_analytics_workspace.workspace.primary_shared_key
+  description = "The primary shared key for accessing and managing the Log Analytics workspace."
+  value       = azurerm_log_analytics_workspace.workspace.primary_shared_key
+  sensitive   = true
 }
 
 output "secondary_shared_key" {
-  value = azurerm_log_analytics_workspace.workspace.secondary_shared_key
+  description = "The secondary shared key for accessing and managing the Log Analytics workspace."
+  value       = azurerm_log_analytics_workspace.workspace.secondary_shared_key
+  sensitive   = true
 }
