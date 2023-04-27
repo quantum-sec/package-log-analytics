d3576be110fc8d35e5344c506eb685f440b599ef
Chris Jaimon <chris.jaimon@quantum.security>
2023-04-27 16:22:10 +0530

    XDR-4369: update data sources dynamic blocks

--- modules/monitor-data-collection/main.tf
+++ modules/monitor-data-collection/main.tf
@@ -21,129 +21,107 @@ resource "azurerm_monitor_data_collection_rule" "dcr" {
   kind                = var.kind
   tags                = var.tags
 
-  data_sources { #TODO: Fix this
-    dynamic "data_import" {
-      for_each = var.data_sources.data_import != null ? [var.data_sources.data_import] : []
-
-      content {
-        event_hub_data_source {
-          name           = data_import.value.event_hub_data_source.name
-          stream         = data_import.value.event_hub_data_source.stream
-          consumer_group = data_import.value.event_hub_data_source.consumer_group
+  dynamic "data_sources" {
+    for_each = var.data_sources != {} ? [var.data_sources] : []
+    content {
+      dynamic "data_import" {
+        for_each = data_sources.value["data_import"] != null ? [data_sources.value["data_import"]] : []
+        content {
+          event_hub_data_source {
+            name           = data_import.value["event_hub_data_source"]["name"]
+            stream         = data_import.value["event_hub_data_source"]["stream"]
+            consumer_group = data_import.value["event_hub_data_source"]["consumer_group"]
+          }
         }
       }
-    }
-
-    dynamic "extension" {
-      for_each = var.data_sources.extension != null ? var.data_sources.extension : []
-
-      content {
-        extension {
-          extension_name     = extension.value.extension_name
-          name               = extension.value.name
-          streams            = extension.value.streams
-          extension_json     = extension.value.extension_json
-          input_data_sources = extension.value.input_data_sources
+      dynamic "extension" {
+        for_each = data_sources.value["extension"] != null ? data_sources.value["extension"] : []
+        content {
+          extension_name     = extension.value["extension_name"]
+          name               = extension.value["name"]
+          streams            = extension.value["streams"]
+          extension_json     = extension.value["extension_json"]
+          input_data_sources = extension.value["input_data_sources"]
         }
       }
-    }
-
-    dynamic "iis_log" {
-      for_each = var.data_sources.iis_log != null ? var.data_sources.iis_log : []
-
-      content {
-        iis_log {
-          name            = iis_log.value.name
-          streams         = iis_log.value.streams
-          log_directories = iis_log.value.log_directories
+      dynamic "iis_log" {
+        for_each = data_sources.value["iis_log"] != null ? data_sources.value["iis_log"] : []
+        content {
+          name            = iis_log.value["name"]
+          streams         = iis_log.value["streams"]
+          log_directories = iis_log.value["log_directories"]
         }
       }
-    }
-
-    dynamic "log_file" {
-      for_each = var.data_sources.log_file != null ? var.data_sources.log_file : []
-
-      content {
-        log_file {
-          name          = log_file.value.name
-          streams       = log_file.value.streams
-          file_patterns = log_file.value.file_patterns
-          format        = log_file.value.format
-          settings = log_file.value.settings != null ? {
-            text = {
-              record_start_timestamp_format = log_file.value.settings.text.record_start_timestamp_format
+      dynamic "log_file" {
+        for_each = data_sources.value["log_file"] != null ? data_sources.value["log_file"] : []
+        content {
+          name          = log_file.value["name"]
+          streams       = log_file.value["streams"]
+          file_patterns = log_file.value["file_patterns"]
+          format        = log_file.value["format"]
+          dynamic "settings" {
+            for_each = log_file.value["settings"] != null ? [log_file.value["settings"]] : []
+            content {
+              text {
+                record_start_timestamp_format = settings.value["text"]["record_start_timestamp_format"]
+              }
             }
-          } : null
+          }
         }
       }
-    }
-
-    dynamic "performance_counter" {
-      for_each = var.data_sources.performance_counter != null ? var.data_sources.performance_counter : []
-
-      content {
-        performance_counter {
-          name                          = performance_counter.value.name
-          counter_specifiers            = performance_counter.value.counter_specifiers
-          sampling_frequency_in_seconds = performance_counter.value.sampling_frequency_in_seconds
-          streams                       = performance_counter.value.streams
+      dynamic "performance_counter" {
+        for_each = data_sources.value["performance_counter"] != null ? data_sources.value["performance_counter"] : []
+        content {
+          name                          = performance_counter.value["name"]
+          counter_specifiers            = performance_counter.value["counter_specifiers"]
+          sampling_frequency_in_seconds = performance_counter.value["sampling_frequency_in_seconds"]
+          streams                       = performance_counter.value["streams"]
         }
       }
-    }
-
-    dynamic "platform_telemetry" {
-      for_each = var.data_sources.platform_telemetry != null ? var.data_sources.platform_telemetry : []
-
-      content {
-        platform_telemetry {
-          name    = platform_telemetry.value.name
-          streams = platform_telemetry.value.streams
+      dynamic "platform_telemetry" {
+        for_each = data_sources.value["platform_telemetry"] != null ? data_sources.value["platform_telemetry"] : []
+        content {
+          name    = platform_telemetry.value["name"]
+          streams = platform_telemetry.value["streams"]
         }
       }
-    }
-
-    dynamic "prometheus_forwarder" {
-      for_each = var.data_sources.prometheus_forwarder != null ? var.data_sources.prometheus_forwarder : []
-
-      content {
-        prometheus_forwarder {
-          name    = prometheus_forwarder.value.name
-          streams = prometheus_forwarder.value.streams
-
-          label_include_filter = prometheus_forwarder.value.label_include_filter != null ? [
-            for label in prometheus_forwarder.value.label_include_filter : {
-              label = label.label
-              value = label.value
+      dynamic "prometheus_forwarder" {
+        for_each = data_sources.value["prometheus_forwarder"] != null ? data_sources.value["prometheus_forwarder"] : []
+        content {
+          name    = prometheus_forwarder.value["name"]
+          streams = prometheus_forwarder.value["streams"]
+          dynamic "label_include_filter" {
+            for_each = prometheus_forwarder.value["label_include_filter"] != null ? prometheus_forwarder.value["label_include_filter"] : []
+            content {
+              label = label_include_filter.value["label"]
+              value = label_include_filter.value["value"]
             }
-          ] : null
+          }
         }
       }
-    }
-
-    dynamic "syslog" {
-      for_each = var.data_sources.syslog != null ? var.data_sources.syslog : {}
-      content {
-        name           = syslog.value.name
-        log_levels     = syslog.value.log_levels
-        facility_names = syslog.value.facility_names
-        streams        = syslog.value.streams
+      dynamic "syslog" {
+        for_each = data_sources.value["syslog"] != null ? data_sources.value["syslog"] : []
+        content {
+          name           = syslog.value["name"]
+          log_levels     = syslog.value["log_levels"]
+          facility_names = syslog.value["facility_names"]
+          streams        = syslog.value["streams"]
+        }
       }
-    }
-
-    dynamic "windows_event_log" {
-      for_each = var.data_sources.windows_event_log != null ? var.data_sources.windows_event_log : {}
-      content {
-        name           = windows_event_log.value.name
-        streams        = windows_event_log.value.streams
-        x_path_queries = windows_event_log.value.x_path_queries
+      dynamic "windows_event_log" {
+        for_each = data_sources.value["windows_event_log"] != null ? data_sources.value["windows_event_log"] : []
+        content {
+          name           = windows_event_log.value["name"]
+          streams        = windows_event_log.value["streams"]
+          x_path_queries = windows_event_log.value["x_path_queries"]
+        }
       }
-    }
-
-    dynamic "windows_firewall_log" {
-      for_each = var.data_sources.windows_firewall_log != null ? var.data_sources.windows_firewall_log : {}
-      content {
-        name    = windows_firewall_log.value.name
-        streams = windows_firewall_log.value.streams
+      dynamic "windows_firewall_log" {
+        for_each = data_sources.value["windows_firewall_log"] != null ? data_sources.value["windows_firewall_log"] : []
+        content {
+          name    = windows_firewall_log.value["name"]
+          streams = windows_firewall_log.value["streams"]
+        }
       }
     }
   }
@@ -159,66 +137,63 @@ resource "azurerm_monitor_data_collection_rule" "dcr" {
     }
   }
 
-  dynamic "destinations" {
-    for_each = [var.destinations]
-    content {
-      dynamic "azure_monitor_metrics" {
-        for_each = [destinations.value["azure_monitor_metrics"]] != null ? [destinations.value["azure_monitor_metrics"]] : []
-        content {
-          name = azure_monitor_metrics.value["name"]
-        }
+  destinations {
+    dynamic "azure_monitor_metrics" {
+      for_each = [var.destinations["azure_monitor_metrics"]] != null ? [var.destinations["azure_monitor_metrics"]] : []
+      content {
+        name = azure_monitor_metrics.value["name"]
       }
-      dynamic "event_hub" {
-        for_each = destinations.value["event_hub"] != null ? destinations.value["event_hub"] : []
-        content {
-          name         = event_hub.value["name"]
-          event_hub_id = event_hub.value["event_hub_id"]
-        }
+    }
+    dynamic "event_hub" {
+      for_each = var.destinations["event_hub"] != null ? var.destinations["event_hub"] : []
+      content {
+        name         = event_hub.value["name"]
+        event_hub_id = event_hub.value["event_hub_id"]
       }
-      dynamic "event_hub_direct" {
-        for_each = destinations.value["event_hub_direct"] != null ? destinations.value["event_hub_direct"] : []
-        content {
-          name         = event_hub_direct.value["name"]
-          event_hub_id = event_hub_direct.value["event_hub_id"]
-        }
+    }
+    dynamic "event_hub_direct" {
+      for_each = var.destinations["event_hub_direct"] != null ? var.destinations["event_hub_direct"] : []
+      content {
+        name         = event_hub_direct.value["name"]
+        event_hub_id = event_hub_direct.value["event_hub_id"]
       }
-      dynamic "log_analytics" {
-        for_each = destinations.value["log_analytics"] != null ? destinations.value["log_analytics"] : []
-        content {
-          name                  = log_analytics.value["name"]
-          workspace_resource_id = log_analytics.value["workspace_resource_id"]
-        }
+    }
+    dynamic "log_analytics" {
+      for_each = var.destinations["log_analytics"] != null ? var.destinations["log_analytics"] : []
+      content {
+        name                  = log_analytics.value["name"]
+        workspace_resource_id = log_analytics.value["workspace_resource_id"]
       }
-      dynamic "monitor_account" {
-        for_each = destinations.value["monitor_account"] != null ? destinations.value["monitor_account"] : []
-        content {
-          name               = monitor_account.value["name"]
-          monitor_account_id = monitor_account.value["monitor_account_id"]
-        }
+    }
+    dynamic "monitor_account" {
+      for_each = var.destinations["monitor_account"] != null ? var.destinations["monitor_account"] : []
+      content {
+        name               = monitor_account.value["name"]
+        monitor_account_id = monitor_account.value["monitor_account_id"]
       }
-      dynamic "storage_blob" {
-        for_each = destinations.value["storage_blob"] != null ? destinations.value["storage_blob"] : []
-        content {
-          name               = storage_blob.value["name"]
-          storage_account_id = storage_blob.value["storage_account_id"]
-          container_name     = storage_blob.value["container_name"]
-        }
+    }
+    dynamic "storage_blob" {
+      for_each = var.destinations["storage_blob"] != null ? var.destinations["storage_blob"] : []
+      content {
+        name               = storage_blob.value["name"]
+        storage_account_id = storage_blob.value["storage_account_id"]
+        container_name     = storage_blob.value["container_name"]
       }
-      dynamic "storage_blob_direct" {
-        for_each = destinations.value["storage_blob_direct"] != null ? destinations.value["storage_blob_direct"] : []
-        content {
-          name               = storage_blob_direct.value["name"]
-          storage_account_id = storage_blob_direct.value["storage_account_id"]
-          container_name     = storage_blob_direct.value["container_name"]
-        }
+    }
+    dynamic "storage_blob_direct" {
+      for_each = var.destinations["storage_blob_direct"] != null ? var.destinations["storage_blob_direct"] : []
+      content {
+        name               = storage_blob_direct.value["name"]
+        storage_account_id = storage_blob_direct.value["storage_account_id"]
+        container_name     = storage_blob_direct.value["container_name"]
       }
-      dynamic "storage_table_direct" {
-        for_each = destinations.value["storage_table_direct"] != null ? destinations.value["storage_table_direct"] : []
-        content {
-          name               = storage_table_direct.value["name"]
-          table_name         = storage_table_direct.value["table_name"]
-          storage_account_id = storage_table_direct.value["storage_account_id"]
-        }
+    }
+    dynamic "storage_table_direct" {
+      for_each = var.destinations["storage_table_direct"] != null ? var.destinations["storage_table_direct"] : []
+      content {
+        name               = storage_table_direct.value["name"]
+        table_name         = storage_table_direct.value["table_name"]
+        storage_account_id = storage_table_direct.value["storage_account_id"]
       }
     }
   }
